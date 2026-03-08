2aeec1138d79b68abae359162332534f34059443
konradhorber <konrad.horber@gmail.com>
2026-03-08 19:28:32 +0100

    Initial Commit

---
+++ runexport/runexportApp.swift
@@ -0,0 +1,17 @@
+//
+//  runexportApp.swift
+//  runexport
+//
+//  Created by Konrad on 08.03.26.
+//
+
+import SwiftUI
+
+@main
+struct runexportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
